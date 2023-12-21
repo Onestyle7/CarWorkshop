190a1ccf95c47d8da0f5c26e5a218a1b4cbe8048
Bartłomiej Kłuś <klusbartek8@gmail.com>
2023-12-21 15:56:14 +0100

    Did carworkshoptests which testing method of encodedName by logic and nullreference

---
+++ CarWorkshop.DomainTests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using CarWorkshop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Domain.Entities.Tests
+{
+    public class CarWorkshopTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            //Arange
+            var carworkshop = new CarWorkshop();
+            carworkshop.Name = "Test Workshop";
+
+            //act
+            carworkshop.EncodeName();
+
+            //assert
+            carworkshop.EncodeName.Should().Be("test-workshop");
+        }
+    }
+}
